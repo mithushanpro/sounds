be0d7f7714aae1a8e4093898781c3eb8f9d49ef7
mithushanpro <116658854+mithushanpro@users.noreply.github.com>
2023-04-11 19:48:37 +0530

    Add files via upload

---
+++ soundsPaly/js/app.js
@@ -0,0 +1,79 @@
+// document.querySelectorAll("button").addEventListener("click", function () {
+//     alert("btn has clicked !");
+// } );
+
+var button = document.querySelectorAll("button");
+
+// Listener for click event
+for (let i = 0; i <button.length ; i++) {
+
+    button[i].addEventListener("click", function(){
+        var clickKey = this.textContent;
+
+        // var audio = new Audio("sounds/tom-4.mp3");
+        // audio.play()
+       playSound(clickKey);
+       animative(clickKey);
+    })
+}
+
+
+// Listener for keypress event
+
+document.addEventListener("keypress", function(event ){
+    var pressedKey = event.key;
+    playSound(pressedKey);
+    animative(pressedKey);
+})
+
+
+function playSound(keypress) {
+    switch (keypress) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3")
+            tom1.play();
+            break;
+
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3")
+            tom2.play();
+            break;
+
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3")
+            tom3.play();
+            break;
+
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3")
+            tom4.play();
+            break;
+
+        case "j":
+            var snare = new Audio("sounds/snare.mp3")
+            snare.play();
+            break;
+
+        case "k":
+            var kick = new Audio("sounds/kick-bass.mp3")
+            kick.play();
+            break;
+
+        case "l":
+            var crash = new Audio("sounds/crash.mp3")
+            crash.play();
+            break;
+
+        default:
+            break;
+    }
+}
+
+function animative(keypress) {
+    var  button = document.querySelector("." + keypress);
+    button.classList.add("pressed-key");
+
+    setTimeout(function(){
+        button.classList.remove("pressed-key");
+    }, 200);
+}
